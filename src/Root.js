eeca3e34ad06772fd240d92696c433600589ebeb
brannonjames <34928176+brannonjames@users.noreply.github.com>
2018-06-25 14:13:09 -0500

    Get started with testing
    * first attempt at implementing testing
    
    * got first test passing
    
    * test if HomePage renders FeedItems

---
+++ src/Root.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import {Provider} from 'react-redux';
+import rootReducer from './store/rootReducer';
+import thunk from 'redux-thunk';
+import {createStore, applyMiddleware, compose} from 'redux';
+import {BrowserRouter} from 'react-router-dom';
+import {setCurrentUser} from './store/auth/actions';
+import {setTokenHeader} from './services/api';
+import jwtDecode from 'jwt-decode';
+
+
+
+export default ({children, initialState={}}) => {
+  const store = createStore(
+    rootReducer,
+    initialState,
+    applyMiddleware(thunk),
+    compose(
+      applyMiddleware(thunk),
+      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+ )
+  
+
+
+  if(localStorage.jwtToken){
+    setTokenHeader(localStorage.jwtToken);
+    try {
+      store.dispatch(setCurrentUser(jwtDecode(localStorage.jwtToken)))
+    } catch(err){
+      store.dispatch(setCurrentUser({}));
+    }
+  }
+
+
+
+  return (
+    <Provider store={store} >
+      <BrowserRouter>
+        {children}
+      </BrowserRouter>
+    </Provider>
+  )
+}
--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,9 @@ import Adapter from 'enzyme-adapter-react-16';
 
 Enzyme.configure({adapter: new Adapter});
 
+const localStorageMock = {
+  getItem: jest.fn(),
+  setItem: jest.fn(),
+  clear: jest.fn()
+};
+global.localStorage = localStorageMock;
